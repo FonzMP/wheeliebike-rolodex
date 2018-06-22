558ad3636956da1c1ec227d847f1a0083ce56f6c
Alex Wilson <fonzmp@gmail.com>
2018-06-22 16:48:49 -0400

    Relocated signup and login logic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,5 +10,41 @@ class UserController < ApplicationController
       erb :"users/show"
     end
   end
+
+  get '/signup' do
+    erb :"users/create"
+  end
+
+  post '/signup' do
+    @user = User.create(username: params[:username], password: params[:password])
+    @user.email = params[:email] if params[:email] != nil
+
+    if @user.save
+      session[:user_id] = @user.id
+
+      redirect "/users/#{@user.slug}"
+    else
+      erb :"users/create"
+    end
+  end
+
+  get '/login' do
+    erb :"users/login"
+  end
+
+  post '/login' do
+    @user = find_user(params)
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "users/#{@user.slug}"
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
   
 end
