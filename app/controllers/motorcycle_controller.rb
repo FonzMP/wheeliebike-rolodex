777be8ac31c649463ff80b8c5deb3d5339cd5049
Alex Wilson <fonzmp@gmail.com>
2018-06-22 12:37:47 -0400

    Created user sign up and login.

---
+++ app/controllers/motorcycle_controller.rb
@@ -0,0 +1,3 @@
+class MotorcycleController < ApplicationController
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
+
+  attr_writer :slug
+
   has_many :motorcycles
+
+  has_secure_password
+
+  # do I need password w/ has_secure_password?
+  validates :username, uniqueness: true
+  validates :username, presence: true
 
   def slug
     @slug = self.username.downcase.gsub(" ", "-")
   end
-  
+
   def self.find_by_slug(slug_name)
     self.all.each do |user|
       if user.slug = slug_name
