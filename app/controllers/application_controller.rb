4547b64762e1a4c89fe531595651dc03e518a6d4
Alex Wilson <fonzmp@gmail.com>
2018-06-25 11:48:43 -0400

    Adding comments to explain and justify code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    #grabs 'welcome' or 'index' page
     erb :welcome
   end
 
   helpers do
 
     def current_user
+      #finds current user based on session
       if session.include?(:user_id)
+        #locates last instance of current_user if stored, if not, locates current user
         @current_user ||= User.find(session[:user_id])
       end
     end
 
     def find_user(params)
+      # finds user based on params of username being passed in
       @user = User.find_by(username: params[:username])
     end
 
